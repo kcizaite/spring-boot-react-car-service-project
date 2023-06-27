801c2c022c5c0a6f9bd116d19a53ad3cc043e425
Karolina Čižaitė <61018680+kcizaite@users.noreply.github.com>
2023-06-27 13:40:12 +0300

    Delete demo-project-ui directory

--- demo-project-ui/src/components/misc/PrivateRoute.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
-
-export function PrivateRoute({ children }) {
-  const { userIsAuthenticated } = useAuth();
-  return userIsAuthenticated() ? children : <Navigate to="/login" />;
-}
